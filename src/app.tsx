af9d433fb654a2fbda23d7fa85f9772125fc6e0b
Indradev <indradevkr8@gmail.com>
2024-12-11 17:07:04 +0545

    webpack config and asset loading

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "../asset/css/styles.css";
+import ReactLogo from "../asset/images/react-logo.png";
+import SantaLogo from "../asset/images/santa.svg";
 
 export const App = () => {
   return (
-    <h2>Hello World!</h2>
+    <>
+      <img src={SantaLogo} alt="santa-logo" height="50" width="50"/>
+      <div className="justify-content-center">
+        <img src={ReactLogo} alt="react-logo" width="200" height="200" />
+        <h2>Hello World!</h2>
+      </div>
+    </>
   )
 };
